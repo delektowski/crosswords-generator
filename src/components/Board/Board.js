9d12a4818df6b45a20e965f645a4b2d8e575063c
Marcin Delektowski <marcin.delektowski.external@atos.net>
2022-03-13 21:19:31 +0100

    Remove unused useEffect

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Square from "../Square/Squre";
 import { CurrentSquareIdContext } from "../../App";
@@ -22,9 +22,6 @@ const BoardElement = styled.div`
 const Board = () => {
   const { scale } = useContext(CurrentSquareIdContext);
 
-  useEffect(() => {
-
-  },[])
   return (
     <>
       <BoardElement scale={scale}>
